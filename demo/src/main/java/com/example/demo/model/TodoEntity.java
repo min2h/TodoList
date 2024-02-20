1eef9e9932b18747eae62bfe8d8486476dba61c8
min2h <mhlee1304@naver.com>
2024-02-20 22:07:49 +0900

    TodoEntity / TodoRepository / TodoService 추가 + JPA

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,8 +14,13 @@ import lombok.NoArgsConstructor;
 @Builder // 디자인 패턴 (클래스없이 오브젝트 생성가능)
 @NoArgsConstructor // 매개변수가 없는 생성자를 구현함.
 @AllArgsConstructor // 클래스의 모든 멤버 변수를 매개변수로 받는 생성자 구현
-@Data // Getter / Setter 자동화
+@Data // Getter_Setter 자동화
+@Entity
+@Table(name = "Todo") // Todo 테이블 매핑
 public class TodoEntity {
+	@Id
+	@GeneratedValue(generator = "system-uuid")   // Id 자동생성 (생성 방식)
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
 	private String id; // 이 오브젝트의 아이디
 	private String userId; // 사용자 아이디
 	private String title; // Todo 제목
---
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.persistence;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.TodoEntity;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{
+	
+	/*
+	 * SELECT * FROM TodoRepository WHERE userId = '{userId}'
+	 */
+	@Query("select * from Todo t where t.userId = ?1")
+	List<TodoEntity> findByUserId(String userId);
+}
