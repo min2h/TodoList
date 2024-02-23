a98269c3ac8940ec829b12a925e7ba41fbfb1aeb
min2h <mhlee1304@naver.com>
2024-02-23 22:12:49 +0900

    update / delete - [Controller, Service]

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,15 +1,16 @@
 package com.example.demo.service;
 
-import java.lang.StackWalker.Option;
+import java.util.Optional;
 import java.util.List;
 
 import org.glassfish.jaxb.core.v2.TODO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
-import com.google.common.base.Optional;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -48,33 +49,54 @@ public class TodoService {
 
 	}
 	
+	
 	public List<TodoEntity> retrieve(final String userId){
 		return repository.findByUserId(userId);
 	}
+	
 	
 	private void validate(final TodoEntity entity) {
 		if(entity == null) {
 			log.warn("Entity cannot be null.");
 			throw new RuntimeException("Entity cannot be null.");
 		}
-		
-		
-		
-		
 		if(entity.getUserId() == null) {
 			log.warn("Unknown user.");
 			throw new RuntimeException("Unknown user.");
 		}
 	}
 	
+	
 	public List<TodoEntity> update(final TodoEntity entity){
 		//1. 검증
 		validate(entity);
 		
 		//2. 넘겨받은 엔티티 가져옴.
-		final Optional<TodoEntity> original = repository.findById(entity.getId());
+		final Optional<TodoEntity> original = repository.findById(entity.getUserId());
 		
 		//3. 변환된 TodoEntity 존재하면 값을 새 entity  값으로 덮음.
-		// todo
+		original.ifPresent(todo ->{
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			//4. 데이터베이스에 값 저장
+			repository.save(todo);
+		});
+		
+		return retrieve(entity.getUserId());
+	}
+	
+	
+	public List<TodoEntity> delete(final TodoEntity entity){
+		//1. 검증
+		validate(entity);
+		
+		try {
+			repository.delete(entity);
+		} catch(Exception e) {
+			log.error("error deleting entity", entity.getId(), e);
+			throw new RuntimeException("error deleting entity" + entity.getId());
+		}
+		return retrieve(entity.getUserId());
 	}
 }
